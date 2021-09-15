683d573bf8701c6e808d8629afd0ca0106d3e194
tobiaiyelokun <60101614+tobiaiyelokun@users.noreply.github.com>
2021-09-15 12:10:59 +0100

    other changes

--- reporter/models.py
+++ reporter/models.py
@@ -7,7 +7,7 @@ from django.contrib.gis.db import models
 class Incidences(models.Model):
 	name = models.CharField(max_length=20)
 	location = models.PointField(srid=4326)
-	objects = models.GeoManager()
+	#objects = models.GeoManager()
 
 	def __unicode__(self):
 		return self.name
@@ -27,5 +27,3 @@ class Counties(models.Model):
 
     class Meta:
         verbose_name_plural = 'Counties'
-    	
-
